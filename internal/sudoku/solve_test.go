9f0772b468ce7a12f991d7e601b36da64fe76a4b
PhilippSchweizer <ph.m.schweizer@gmail.com>
2026-01-27 15:22:01 +0100

    [test] add test case for solving difficult puzzle

--- internal/sudoku/solve_test.go
+++ internal/sudoku/solve_test.go
@@ -27,6 +27,24 @@ func TestSolve(t *testing.T) {
 			t.Error("should recognize alread solved board as solved")
 		}
 	})
+
+	t.Run("difficult", func(t *testing.T) {
+		b := Board{
+			{0, 1, 6, 0, 8, 0, 5, 3, 0},
+			{0, 0, 4, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 9, 0, 0, 0, 8, 0},
+			{4, 0, 0, 0, 1, 0, 0, 0, 0},
+			{9, 0, 0, 0, 0, 0, 3, 0, 0},
+			{0, 3, 1, 7, 0, 0, 0, 0, 6},
+			{0, 0, 0, 0, 0, 2, 0, 0, 7},
+			{3, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 5, 8, 0, 9, 0, 6, 0, 0},
+		}
+		_, solved := Solve(b)
+		if !solved {
+			t.Error("should be able to solve difficult sudoku puzzle")
+		}
+	})
 }
 
 func TestCountSolutions(t *testing.T) {
