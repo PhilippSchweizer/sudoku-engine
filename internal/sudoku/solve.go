7c79a39b023395bb11a7fe56640e8a2c99a16975
PhilippSchweizer <ph.m.schweizer@gmail.com>
2026-01-27 12:16:21 +0100

    [solve] add mechanism to detect non unique sudoku puzzles

--- internal/sudoku/solve.go
+++ internal/sudoku/solve.go
@@ -22,6 +22,32 @@ func Solve(b Board) (Board, bool) {
 	return b, false
 }
 
+func CountSolutions(b Board) int {
+	if BoardSolved(b) {
+		return 1
+	}
+
+	if !BoardValid(b) {
+		return 0
+	}
+
+	emptyCell := findNextEmpty(b)
+	if emptyCell[0] == -1 {
+		return 1
+	}
+	count := 0
+
+	for c := 1; c <= 9; c++ {
+		newBoard := Set(b, emptyCell[0], emptyCell[1], c)
+		subCount := CountSolutions(newBoard)
+		count += subCount
+		if count >= 2 {
+			return count
+		}
+	}
+	return count
+}
+
 func findNextEmpty(b Board) [2]int {
 	for row := range len(b) {
 		for col := range len(b) {
