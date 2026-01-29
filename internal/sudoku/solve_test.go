f01f684ff50ea5582c4f380172fab30132d25091
PhilippSchweizer <ph.m.schweizer@gmail.com>
2026-01-29 15:59:43 +0100

    [test] improve test coverage

--- internal/sudoku/solve_test.go
+++ internal/sudoku/solve_test.go
@@ -4,7 +4,11 @@ import "testing"
 
 func TestSolve(t *testing.T) {
 	t.Run("solve_empty", func(t *testing.T) {
-		_, solved := Solve(New())
+		result, solved := Solve(New())
+		if !BoardSolved(result) || !BoardValid(result) {
+			t.Error("Board solved by Solve() should be complete and valid.")
+		}
+
 		if !solved {
 			t.Error("should be able to solve empty board")
 		}
@@ -27,6 +31,26 @@ func TestSolve(t *testing.T) {
 		_, solved := Solve(b)
 		if !solved {
 			t.Error("should recognize alread solved board as solved")
+		}
+	})
+
+	t.Run("invalid_board", func(t *testing.T) {
+		b := Board{
+			Cells: [9][9]int{
+				{5, 3, 4, 6, 7, 8, 9, 1, 2},
+				{6, 7, 2, 1, 9, 5, 3, 4, 8},
+				{1, 9, 8, 3, 4, 2, 5, 6, 7},
+				{8, 5, 9, 7, 6, 1, 4, 2, 3},
+				{4, 2, 6, 8, 5, 3, 7, 9, 1},
+				{7, 1, 3, 9, 2, 4, 8, 5, 6},
+				{9, 6, 1, 5, 3, 7, 2, 8, 4},
+				{2, 8, 7, 4, 1, 9, 6, 3, 5},
+				{3, 4, 5, 2, 8, 1, 1, 7, 9},
+			},
+		}
+		_, solved := Solve(b)
+		if solved {
+			t.Error("Invalid board should be identified as such by Solve().")
 		}
 	})
 }
