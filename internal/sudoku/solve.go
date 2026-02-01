04c1e5667d2f19fff9d61ca7988ffda1d06b2382
PhilippSchweizer <ph.m.schweizer@gmail.com>
2026-02-01 17:41:55 +0100

    [solve] add Hidden Pair Strategy

--- internal/sudoku/solve.go
+++ internal/sudoku/solve.go
@@ -124,6 +124,27 @@ func (b Board) nakedPair() (found bool, row1, col1, row2, col2 int, val1, val2 i
 	return false, -1, -1, -1, -1, 0, 0
 }
 
+func (b Board) hiddenPair() (found bool, row1, col1, row2, col2 int, val1, val2 int) {
+	for r := range 9 {
+		if found, r1, c1, r2, c2, v1, v2 := b.hiddenPairInUnit(func(i int) (int, int) { return r, i }); found {
+			return true, r1, c1, r2, c2, v1, v2
+		}
+	}
+	for c := range 9 {
+		if found, r1, c1, r2, c2, v1, v2 := b.hiddenPairInUnit(func(i int) (int, int) { return i, c }); found {
+			return true, r1, c1, r2, c2, v1, v2
+		}
+	}
+	for box := range 9 {
+		br := (box / 3) * 3
+		bc := (box % 3) * 3
+		if found, r1, c1, r2, c2, v1, v2 := b.hiddenPairInUnit(func(i int) (int, int) { return br + i/3, bc + i%3 }); found {
+			return true, r1, c1, r2, c2, v1, v2
+		}
+	}
+	return false, -1, -1, -1, -1, 0, 0
+}
+
 func (b Board) hiddenSingleInUnit(getCell func(i int) (row, col int)) (found bool, row, col, val int) {
 	for v := 1; v <= 9; v++ {
 		count := 0
@@ -147,7 +168,7 @@ func (b Board) hiddenSingleInUnit(getCell func(i int) (row, col int)) (found boo
 
 func (b Board) nakedPairInUnit(getCell func(i int) (row, col int)) (found bool, row1, col1, row2, col2, val1, val2 int) {
 	// Find two empty cells in the unit that have exactly the same two candidates.
-	for i := 0; i < 9; i++ {
+	for i := range 9 {
 		row, col := getCell(i)
 		if b.Cell(row, col) != 0 {
 			continue
@@ -169,6 +190,42 @@ func (b Board) nakedPairInUnit(getCell func(i int) (row, col int)) (found bool, 
 			v1 := bits.TrailingZeros16(mask) + 1
 			v2 := bits.TrailingZeros16(mask&^(uint16(1)<<(v1-1))) + 1
 			return true, row, col, rowOther, colOther, v1, v2
+		}
+	}
+	return false, -1, -1, -1, -1, 0, 0
+}
+
+func (b Board) hiddenPairInUnit(getCell func(i int) (row, col int)) (found bool, row1, col1, row2, col2, val1, val2 int) {
+	type coord struct{ r, c int }
+	positions := [9][]coord{}
+	for p := range 9 {
+		row, col := getCell(p)
+		if b.Cell(row, col) != 0 {
+			continue
+		}
+		for v := 1; v <= 9; v++ {
+			if b.HasCandidate(row, col, v) {
+				pos := coord{row, col}
+				positions[v-1] = append(positions[v-1], pos)
+			}
+		}
+	}
+
+	for v1 := range 9 {
+		if len(positions[v1]) != 2 {
+			continue
+		}
+		for v2 := v1 + 1; v2 < 9; v2++ {
+			if len(positions[v2]) != 2 {
+				continue
+			}
+			if positions[v1][0] == positions[v2][0] && positions[v1][1] == positions[v2][1] {
+				c1 := positions[v1][0]
+				row1, col1 = c1.r, c1.c
+				c2 := positions[v1][1]
+				row2, col2 = c2.r, c2.c
+				return true, row1, col1, row2, col2, v1 + 1, v2 + 1
+			}
 		}
 	}
 	return false, -1, -1, -1, -1, 0, 0
@@ -242,6 +299,22 @@ func (b *Board) ApplyNakedPair() (applied bool) {
 			b.RemoveCandidate(br+i/3, bc+i%3, val1)
 			b.RemoveCandidate(br+i/3, bc+i%3, val2)
 		}
+	}
+	return true
+}
+
+func (b *Board) ApplyHiddenPair() (applied bool) {
+	found, row1, col1, row2, col2, val1, val2 := b.hiddenPair()
+	if !found {
+		return false
+	}
+	for v := 1; v <= 9; v++ {
+		if v == val1 || v == val2 {
+			continue
+		}
+		b.RemoveCandidate(row1, col1, v)
+		b.RemoveCandidate(row2, col2, v)
+
 	}
 	return true
 }
